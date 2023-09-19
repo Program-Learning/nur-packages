89ec43946b65be655e1318c2aae85cd51d8eb093
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-09-20 01:23:51 +0800

    Add desktopItems

--- pkgs/wechat_dev_tools_appimage/wechat_dev_tools.nix
+++ pkgs/wechat_dev_tools_appimage/wechat_dev_tools.nix
@@ -42,24 +42,37 @@ stdenv.mkDerivation rec {
       --add-flags "$out/Appimage/wechat_dev_tools.AppImage"
   '';
 
-  wechat_dev_tools-desktop =
-    pkgs.writeText "share/applications/wechat_dev_tools-desktop.desktop" ''
-      [Desktop Entry]
-      Name=WeChat Dev Tools
-      Name[zh_CN]=微信开发者工具
-      Comment=The development tools for wechat projects
-      Comment[zh_CN]=提供微信开发相关项目的开发IDE支持
-      Categories=Development;WebDevelopment;IDE;
-      Exec=${appimage-run}/bin/appimage-run $out/bin/wechat_dev_tools
-      Icon=wechat-devtools
-      Type=Application
-      Terminal=false
-      StartupWMClass=wechat_devtools
-      Actions=
-      MimeType=x-scheme-handler/wechatide
-      X-AppImage-Version=v$version
+  # wechat_dev_tools-desktop =
+  #   pkgs.writeText "share/applications/wechat_dev_tools-desktop.desktop" ''
+  #     [Desktop Entry]
+  #     Name=WeChat Dev Tools
+  #     Name[zh_CN]=微信开发者工具
+  #     Comment=The development tools for wechat projects
+  #     Comment[zh_CN]=提供微信开发相关项目的开发IDE支持
+  #     Categories=Development;WebDevelopment;IDE;
+  #     Exec=${appimage-run}/bin/appimage-run $out/bin/wechat_dev_tools
+  #     Icon=wechat-devtools
+  #     Type=Application
+  #     Terminal=false
+  #     StartupWMClass=wechat_devtools
+  #     Actions=
+  #     MimeType=x-scheme-handler/wechatide
+  #     X-AppImage-Version=v$version
 
-    '';
+  #   '';
+
+  desktopItems = lib.toList (makeDesktopItem {
+    name = "Wechat Dev Tools";
+    desktopName = "Wechat Dev Tools";
+    genericName = "The development tools for wechat projects";
+    exec = "${appimage-run}/bin/appimage-run $out/bin/wechat_dev_tools";
+    icon = "wechat-devtools";
+    comment = "The development tools for wechat projects";
+    mimeTypes = [ "x-scheme-handler/wechatide" ];
+    desktopName = "Wechat Dev Tools";
+    categories = [ "Development" "WebDevelopment" "IDE" ];
+    startupWMClass = "wechat_devtools";
+  });
   meta = with lib; {
     description = "Wechat Dev Tools";
     homepage = "https://github.com/msojocs/wechat-web-devtools-linux";
