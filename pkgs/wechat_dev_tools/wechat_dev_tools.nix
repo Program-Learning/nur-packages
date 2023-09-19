0a68b95d305ac587681973ced1a41778df09fe58
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-09-19 21:17:13 +0800

    Test

--- pkgs/wechat_dev_tools/wechat_dev_tools.nix
+++ pkgs/wechat_dev_tools/wechat_dev_tools.nix
@@ -1,15 +1,11 @@
-{
-  fetchurl,
-  lib,
-  stdenv,
-  pkgs,
-}:
+{ fetchurl, lib, stdenv, pkgs, }:
 stdenv.mkDerivation rec {
   pname = "wechat_dev_tools";
   version = "1.06.2307260-1";
 
   src = fetchurl {
-    url = "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/io.github.msojocs.wechat-devtools-linux_${version}_amd64.deb";
+    url =
+      "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/io.github.msojocs.wechat-devtools-linux_${version}_amd64.deb";
     sha256 = "sha256-lwjGqUBNmJRhF+mEI2Skd5tOPWkvzP3gOwJGr8m/LBw=";
   };
   sourceRoot = ".";
@@ -21,7 +17,8 @@ stdenv.mkDerivation rec {
     dpkg
   ];
 
-  unpackPhase = "dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner";
+  unpackPhase =
+    "dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner";
 
   installPhase = ''
     _package-ide() {
@@ -36,13 +33,28 @@ stdenv.mkDerivation rec {
     _package-ide
   '';
 
+  libraries = [ libdrm nspr alsaLib ];
+
+  buildInputs = libraries;
+
+  #   runtimeLibs = pkgs.lib.makeLibraryPath [
+  #   pkgs.libudev0-shim
+  #   pkgs.glibc
+  #   pkgs.libsecret
+  #   pkgs.nss
+  # ];
+
+  # preFixup = ''
+  #   makeWrapper $out/share/feishu/feishu $out/bin/feishu \
+  #     --prefix LD_LIBRARY_PATH : "${runtimeLibs}" \
+  #     "''${gappsWrapperArgs[@]}"
+  # '';
+
   meta = with lib; {
     description = "Wechat Dev Tools";
     homepage = "https://github.com/msojocs/wechat-web-devtools-linux";
     license = licenses.unfree;
-    platforms = [
-      "x86_64-linux"
-    ];
-    maintainers = with maintainers; [Program-Learning];
+    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ Program-Learning ];
   };
 }
