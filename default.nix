d3f6d34934767395a9b83ea6b354b8d705c0e0e2
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-10-29 00:35:39 +0800

    Snap

--- default.nix
+++ default.nix
@@ -34,6 +34,7 @@
       true;
   };
   watt-toolkit_2 = pkgs.callPackage ./pkgs/watt-toolkit_2 { };
+  AppimageLauncher = pkgs.callPackage ./pkgs/AppimageLauncher { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
