02a296ecec99c76a1b771d13641e07e8784b2adb
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-19 03:47:54 +0800

    Test

--- pkgs/clang_dev_env_fhs/default.nix
+++ pkgs/clang_dev_env_fhs/default.nix
@@ -1,13 +1,19 @@
-{ buildFHSEnvChroot, ... }:
-buildFHSEnvChroot {
-  name = "clang_dev_env_fhs";
-  targetPkgs = pkgs:
-    with pkgs; [
-      clang-tools
-      ccls
-      llvmPackages.clang # c/c++ tools with clang-tools such as clangd
-    ];
-  profile = ''
-    export FHS=1
-  '';
-}
+{pkgs ? import <nixpkgs> {}}: let
+  fhs = pkgs.buildFHSEnvChroot {
+    name = "clang_dev_env_fhs";
+    targetPkgs = pkgs:
+      with pkgs; [
+        clang-tools
+        ccls
+        llvmPackages.clang # c/c++ tools with clang-tools such as clangd
+      ];
+    profile = ''
+      export FHS=1
+    '';
+  };
+in
+  pkgs.stdenv.mkDerivation {
+    name = "clang_dev_env_fhs-shell";
+    nativeBuildInputs = [fhs];
+    shellHook = "exec clang_dev_env_fhs";
+  }
