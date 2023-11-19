edd90c30fd2dc5b26d0da8e479fa9d96ae6e8b99
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-19 22:57:44 +0800

    Test

---
+++ pkgs/llqqnt/llqqnt.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+let
+  LiteLoaderQQNT_SRC = builtins.fetchGit {
+    url = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
+    rev = "4fad0cb92d6881438abfa0ffc2934689b6828dd3";
+    submodules = true;
+  };
+in
+pkgs.qq.overrideAttrs (oldAttrs: {
+  pname = "llqqnt";
+
+  postInstall = ''
+    mkdir $out/opt/QQ/resources/app/LiteLoader
+    cp -r ${LiteLoaderQQNT_SRC.outPath}/* $out/opt/QQ/resources/app/LiteLoader
+    sed -i 's/"main": ".\/app_launcher\/index.js"/"main": ".\/LiteLoader"/' $out/opt/QQ/resources/app/package.json
+  '';
+})
