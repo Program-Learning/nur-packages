925b3828b53fd77b516c40ea53c686aea0682579
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-19 23:32:31 +0800

    Test

--- pkgs/llqqnt/llqqnt.nix
+++ pkgs/llqqnt/llqqnt.nix
@@ -1,19 +1,22 @@
 #https://github.com/Prismwork/llqqnt-nix/blob/trunk/pkgs/llqqnt.nix
-{ pkgs, ... }:
-
-let
-  LiteLoaderQQNT_SRC = builtins.fetchGit {
-    url = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
-    rev = "06104e1d88131bb668129443aa8f1d2a18ebbf05";
-    submodules = true;
+{
+  pkgs,
+  fetchurl,
+  ...
+}: let
+  # https://aur.archlinux.org/packages/liteloader-qqnt-bin
+  LiteLoaderQQNT_VERSION = "0.5.9";
+  LiteLoaderQQNT_SRC = fetchurl {
+    url = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/${LiteLoaderQQNT_VERSION}/LiteLoaderQQNT.zip";
+    sha256 = "";
   };
 in
-pkgs.qq.overrideAttrs (oldAttrs: {
-  pname = "llqqnt";
+  pkgs.qq.overrideAttrs (oldAttrs: {
+    pname = "llqqnt";
 
-  postInstall = ''
-    mkdir $out/opt/QQ/resources/app/LiteLoader
-    cp -r ${LiteLoaderQQNT_SRC.outPath}/* $out/opt/QQ/resources/app/LiteLoader
-    sed -i 's/"main": ".\/app_launcher\/index.js"/"main": ".\/LiteLoader"/' $out/opt/QQ/resources/app/package.json
-  '';
-})
+    postInstall = ''
+      mkdir $out/opt/QQ/resources/app/LiteLoader
+      cp -r ${LiteLoaderQQNT_SRC.outPath}/* $out/opt/QQ/resources/app/LiteLoader
+      sed -i 's/"main": ".\/app_launcher\/index.js"/"main": ".\/LiteLoader"/' $out/opt/QQ/resources/app/package.json
+    '';
+  })
