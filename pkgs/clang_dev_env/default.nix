4d800f20709bd475a9532260f45431a684e4c601
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-19 13:18:12 +0800

    Test

--- pkgs/clang_dev_env/default.nix
+++ pkgs/clang_dev_env/default.nix
@@ -15,6 +15,10 @@ in
   pkgs.stdenv.mkDerivation {
     name = "clang_dev_env_fhs-shell";
     nativeBuildInputs = [fhs];
-    src = builtins.path { path = ./.; };
+    src = builtins.path {path = ./.;};
+    installPhase = ''
+      mkdir --mode=0755 --parent $out/bin
+      cp -r ${fhs}/bin "$out"
+    '';
     shellHook = "exec clang_dev_env_fhs";
   }
