6e3f4e62ae2606bb133b626df1cfc49d75cd3286
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-01-03 02:18:32 +0800

    Try to fix

--- pkgs/xcursor-genshin-nahida/xcursor-genshin-nahida.nix
+++ pkgs/xcursor-genshin-nahida/xcursor-genshin-nahida.nix
@@ -1,11 +1,7 @@
 {
   lib,
   stdenvNoCC,
-  fetchFromGitHub,
-  fetchurl,
   fetchgit,
-  clickgen,
-  attrs,
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "xcursor-genshin-nahida";
@@ -14,18 +10,25 @@ stdenvNoCC.mkDerivation rec {
   src = fetchgit {
     url = "https://aur.archlinux.org/xcursor-genshin-nahida.git";
     rev = "3a6d21a337925f47466c74d16c413e7be6ee58e4";
-    hash = "";
+    hash = "sha256-I8UVUlfoqVXRstehGyFumq8oE5cLxRs6DKvcHol1AQk=";
   };
 
+  dontBuild = true;
+
   installPhase = ''
-    tar -xf xcursor-genshin-nahida.tar.gz --directory=$out
+    runHook preInstall
+
+    mkdir -p $out/share/icons
+    tar -xf xcursor-genshin-nahida.tar.gz --directory=$out/share/icons
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "xcursor genshin nahida";
     homepage = "https://aur.archlinux.org/packages/xcursor-genshin-nahida";
-    license = licenses.gpl;
-    # maintainers = with maintainers; [ Program-Learning ];
     platforms = platforms.linux;
+    license = licenses.gpl3;
+    # maintainers = with maintainers; [ Program-Learning ];
   };
 }
