c2bde7f74d9f2b25cc182de0893433b314712529
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-01-25 01:43:57 +0800

    Test

--- pkgs/llqqnt/llqqnt.nix
+++ pkgs/llqqnt/llqqnt.nix
@@ -1,20 +1,22 @@
 #https://github.com/Prismwork/llqqnt-nix/blob/trunk/pkgs/llqqnt.nix
 {
   pkgs,
-  fetchurl,
-  fetchgit,
   ...
 }: let
   LiteLoaderQQNT_VERSION = "1.0.2";
+  LiteLoaderQQNT_REV = "f621aeef07b77aa037b46af00aee8055ffb006ef";
   LiteLoaderQQNT_URL = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
-  LiteLoaderQQNT_SRC = fetchgit {
+  LiteLoaderQQNT_SRC = fetchGit {
     url = LiteLoaderQQNT_URL;
-    sha256 = "sha256-4pWXd/C3Fgh0kQjHOdN1Dce82d9WQN7r/21M0W+JE5Y=";
+    rev = LiteLoaderQQNT_REV;
+    submodules = true;
   };
 in
-  pkgs.qq.overrideAttrs (oldAttrs @ {nativeBuildInputs ? [],version, ...}: {
-    nativeBuildInputs = nativeBuildInputs ++ [pkgs.p7zip];
-
+  pkgs.qq.overrideAttrs (oldAttrs @ {
+    nativeBuildInputs ? [],
+    version,
+    ...
+  }: {
     postInstall = ''
       sed -i '1s@^@require("${LiteLoaderQQNT_SRC}");\n@' $out/opt/QQ/resources/app/app_launcher/index.js
       mkdir -vp $out/opt/QQ/resources/app/application/
