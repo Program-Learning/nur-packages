25e0284badf2022d8dcfadfc9cf6869e644d73c9
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-01-29 21:27:27 +0800

    SnapShot

--- pkgs/llqqnt/default.nix
+++ pkgs/llqqnt/default.nix
@@ -1,1 +1,18 @@
-import ./llqqnt.nix
+{
+  buildFHSEnvChroot,
+  callPackage,
+}: let
+  llqqnt = callPackage ./llqqnt.nix {};
+in
+  buildFHSEnvChroot {
+    name = "qq";
+    targetPkgs = pkgs:
+      with pkgs; [
+        llqqnt
+      ];
+    profile = ''
+      export FHS=1
+      export  LITELOADERQQNT_PROFILE=$XDG_DATA_HOME/LLQQNT
+      qq
+    '';
+  }
