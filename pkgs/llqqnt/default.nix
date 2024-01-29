68a7d23ba37cc445735e7c4729b9bbbff70e5328
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-01-29 22:25:21 +0800

    SnapShot

--- pkgs/llqqnt/default.nix
+++ pkgs/llqqnt/default.nix
@@ -1,18 +1,1 @@
-{
-  buildFHSEnvChroot,
-  callPackage,
-}: let
-  llqqnt = callPackage ./llqqnt.nix {};
-in
-  buildFHSEnvChroot {
-    name = "qq";
-    targetPkgs = pkgs:
-      with pkgs; [
-        llqqnt
-      ];
-    profile = ''
-      export FHS=1
-      export LITELOADERQQNT_PROFILE=$XDG_DATA_HOME/LLQQNT
-      qq
-    '';
-  }
+import ./llqqnt.nix
--- pkgs/llqqnt/llqqnt.nix
+++ pkgs/llqqnt/llqqnt.nix
@@ -1,8 +1,5 @@
 #https://github.com/Prismwork/llqqnt-nix/blob/trunk/pkgs/llqqnt.nix
-{
-  pkgs,
-  ...
-}: let
+{pkgs, ...}: let
   LiteLoaderQQNT_VERSION = "1.0.2";
   LiteLoaderQQNT_REV = "f621aeef07b77aa037b46af00aee8055ffb006ef";
   LiteLoaderQQNT_URL = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
@@ -11,15 +8,36 @@
     rev = LiteLoaderQQNT_REV;
     submodules = true;
   };
+  fhs =
+    # create a fhs environment by command `fhs`, so we can run non-nixos packages in nixos!
+    (
+      let
+        base = pkgs.appimageTools.defaultFhsEnvArgs;
+      in
+        pkgs.buildFHSUserEnv (base
+          // {
+            name = "fhs";
+            targetPkgs = pkgs: (base.targetPkgs pkgs) ++ [pkgs.pkg-config];
+            profile = "export FHS=1";
+            runScript = "bash";
+            extraOutputsToInstall = ["dev"];
+          })
+    );
 in
   pkgs.qq.overrideAttrs (oldAttrs @ {
     nativeBuildInputs ? [],
     version,
     ...
   }: {
+    nativeBuildInputs =
+      nativeBuildInputs
+      ++ [
+        fhs
+      ];
     postInstall = ''
       sed -i '1s@^@require("${LiteLoaderQQNT_SRC}");\n@' $out/opt/QQ/resources/app/app_launcher/index.js
       mkdir -vp $out/opt/QQ/resources/app/application/
+      sed 's@Exec=@Exec = env LITELOADERQQNT_PROFILE=~/.local/share/LLQQNT fhs -c @g' $out/share/applications/qq.desktop
       cp -f ${LiteLoaderQQNT_SRC}/src/preload.js $out/opt/QQ/resources/app/application/
     '';
   })
