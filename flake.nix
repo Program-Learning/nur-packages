8ca4395cbcd51c00830327aba1bec888b8338782
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-06-08 16:47:21 +0800

    fix: some param not exist

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
     {
       legacyPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
-        inherit system;
         inherit inputs;
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
