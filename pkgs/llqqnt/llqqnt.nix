820a0aeded7cfa963026a8d2809a24fe663e5ae7
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-07-06 18:31:32 +0800

    chore: update llqqnt derive

--- pkgs/llqqnt/llqqnt.nix
+++ pkgs/llqqnt/llqqnt.nix
@@ -1,11 +1,7 @@
 #https://github.com/Prismwork/llqqnt-nix/blob/trunk/pkgs/llqqnt.nix
-{
-  pkgs,
-  commandLineArgs ? "",
-  ...
-}: let
-  LiteLoaderQQNT_VERSION = "1.1.1";
-  LiteLoaderQQNT_REV = "6f1972d2bc83fe1a1d26e82a49ee15d8cc079018";
+{ pkgs, appimageTools, buildFHSUserEnv, pkg-config, qq, ... }:
+let
+  LiteLoaderQQNT_REV = "af1d19abd92d11f2d0316a940592593f3b254705";
   LiteLoaderQQNT_URL = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT";
   LiteLoaderQQNT_SRC = fetchGit {
     url = LiteLoaderQQNT_URL;
@@ -15,56 +11,58 @@
   };
   fhs =
     # create a fhs environment by command `fhs`, so we can run non-nixos packages in nixos!
-    (
-      let
-        base = pkgs.appimageTools.defaultFhsEnvArgs;
-      in
-        pkgs.buildFHSUserEnv (base
-          // {
-            name = "fhs";
-            targetPkgs = pkgs: (base.targetPkgs pkgs) ++ [pkgs.pkg-config];
-            profile = "export FHS=1";
-            runScript = "bash";
-            extraOutputsToInstall = ["dev"];
-          })
+    let
+      base = appimageTools.defaultFhsEnvArgs;
+    in
+    buildFHSUserEnv (
+      base
+      // {
+        name = "fhs";
+        targetPkgs = pkgs: (base.targetPkgs pkgs) ++ [ pkg-config ];
+        profile = "export FHS=1";
+        runScript = "bash";
+        extraOutputsToInstall = [ "dev" ];
+      }
     );
 in
-  (pkgs.qq.override {
-    # can not work and i have no idea
-    # inherit commandLineArgs;
-  })
-  .overrideAttrs (oldAttrs @ {
-    nativeBuildInputs ? [],
-    installPhase,
-    version,
-    ...
-  }: {
-    nativeBuildInputs =
-      nativeBuildInputs
-      ++ [
-        fhs
-      ];
-    installPhase =
-      builtins.replaceStrings [
-        ''
-          makeShellWrapper $out/opt/QQ/qq $out/bin/qq \
-        ''
-        "--enable-features=WaylandWindowDecorations"
-      ] [
-        ''
-          makeShellWrapper $out/opt/QQ/qq $out/bin/qq \
-            --prefix LITELOADERQQNT_PROFILE_TEST : ''${XDG_DATA_HOME:-~/.local/share}/LLQQNT \
-            --prefix LITELOADERQQNT_PROFILE_TEST2 : ~/.local/share/LLQQNT \
-        ''
-        "--enable-features=WaylandWindowDecorations --enable-wayland-ime"
-      ]
-      installPhase;
-    postInstall = ''
-      # Patch QQ
-      sed -i "1s@^@require(String.raw\`${LiteLoaderQQNT_SRC}\`);\n@" $out/opt/QQ/resources/app/app_launcher/index.js
-      mkdir -vp $out/opt/QQ/resources/app/application/
-      cp -f ${LiteLoaderQQNT_SRC}/src/preload.js $out/opt/QQ/resources/app/application/
-      # Use FHS environment run Patched QQ
-      sed -i "s@^Exec=.*@Exec=${fhs}/bin/fhs -c 'LITELOADERQQNT_PROFILE=~/.local/share/LLQQNT $out/bin/qq %U'@g" $out/share/applications/qq.desktop
-    '';
-  })
+(qq.override {
+  # can not work and i have no idea
+  # inherit commandLineArgs;
+}).overrideAttrs
+  (
+    {
+      # nativeBuildInputs ? [ ],
+      runtimeDependencies ? [ ],
+      # buildInputs ? [ ],
+      installPhase,
+      ...
+    }:
+    {
+      runtimeDependencies = runtimeDependencies ++ [ fhs ];
+      installPhase =
+        builtins.replaceStrings
+          [
+            ''
+              makeShellWrapper $out/opt/QQ/qq $out/bin/qq \
+            ''
+            "--enable-features=WaylandWindowDecorations"
+          ]
+          [
+            ''
+              makeShellWrapper $out/opt/QQ/qq $out/bin/llqqnt \
+                --prefix LITELOADERQQNT_PROFILE_TEST : ''${XDG_DATA_HOME:-~/.local/share}/LLQQNT \
+                --prefix LITELOADERQQNT_PROFILE_TEST2 : ~/.local/share/LLQQNT \
+            ''
+            "--enable-features=WaylandWindowDecorations --enable-wayland-ime"
+          ]
+          installPhase;
+      postInstall = ''
+        # Patch QQ
+        sed -i "1s@^@require(String.raw\`${LiteLoaderQQNT_SRC}\`);\n@" $out/opt/QQ/resources/app/app_launcher/index.js
+        mkdir -vp $out/opt/QQ/resources/app/application/
+        cp -f ${LiteLoaderQQNT_SRC}/src/preload.js $out/opt/QQ/resources/app/application/
+        # Use FHS environment run Patched QQ
+        sed -i "s@^Exec=.*@Exec=${fhs}/bin/fhs -c 'LITELOADERQQNT_PROFILE=~/.local/share/LLQQNT $out/bin/llqqnt %U'@g" $out/share/applications/qq.desktop
+      '';
+    }
+  )
